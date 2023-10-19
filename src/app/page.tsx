47c2f219a13c8feb689ca2d314912c969ee6e2c4
BenSmith09 <115125244+BenSmith09@users.noreply.github.com>
2023-10-20 08:24:14 +1000

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-gradient-to-r min-h-screen from-rose-100 to-teal-100"></div>
+  );
+}
---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <SignUp />
+    </div>
+  );
+}
